3fac19825a816f1b62b19c04bea468a85d57d711
Suetosha <honey.pancake2017@yandex.ru>
2025-12-17 22:17:34 +0100

    Simplify routing URLs for categories and tasks APIs by removing path prefixes.

--- backend/apps/tasks/urls.py
+++ backend/apps/tasks/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
 
 router = DefaultRouter()
-router.register(r"tasks", TaskViewSet, basename="task")
+router.register(r"", TaskViewSet, basename="task")
 
 urlpatterns = [
     path("", include(router.urls)),
