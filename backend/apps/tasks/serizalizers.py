301e8b8280ab027d93d6c0bbf7a08a539c522266
Suetosha <honey.pancake2017@yandex.ru>
2025-12-17 17:28:19 +0100

    Add TaskSerializer, TaskViewSet, and API routing for tasks management.

---
+++ backend/apps/tasks/serizalizers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from .models import Task
+from ..categories.models import Category
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    categories = serializers.PrimaryKeyRelatedField(
+        many=True,
+        required=False,
+        queryset=Category.objects.none()
+    )
+
+    class Meta:
+        model = Task
+        fields = (
+            "id",
+            "title",
+            "description",
+            "due_at",
+            "is_completed",
+            "categories",
+            "created_at",
+            "updated_at",
+        )
+        read_only_fields = ("id", "created_at", "updated_at")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        request = self.context.get("request")
+        if request and request.user and request.user.is_authenticated:
+            self.fields["categories"].queryset = Category.objects.filter(user=request.user)
+        else:
+            self.fields["categories"].queryset = Category.objects.none()
